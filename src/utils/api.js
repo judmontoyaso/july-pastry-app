2bee0d2989ded7a48a10e1085211a52fe22736c5
Juan David Montoya <82336705+judmontoyaso@users.noreply.github.com>
2023-01-25 17:15:43 +0000

    Cambiar url de api por despliegue en railway

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "api-julypastry-production.up.railway.app"
+const baseURL = "https://api-julypastry-production.up.railway.app"
 
 const getToken = () => {
   return `Bearer ${localStorage.getItem("token")}`;
